a70f6435058977b63baa345e4ffabce9019af6a9
KM <18610929200@163.com>
2018-10-15 16:41:30 +0800

    info part implement

--- albums/src/compoment/AlbumDetail.js
+++ albums/src/compoment/AlbumDetail.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import {Image,Text} from 'react-native';
+import {Image,Text,View} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+    const {image,thumbnail,title,des} = album;
+
+    const {
+        imageStyle,
+        imageContainerStyle,
+        thumbnailStyle,
+        titleStyle,
+        infoStyle,
+        desStyle
+    } = styles;
+
     return (
         <Card>
-            <CardSection style={styles.imageContainerStyle}> 
-                <Image resizeMode='cover' source={{url:props.album.image}} style={styles.imageStyle}></Image>
+            <CardSection style={imageContainerStyle}> 
+                <Image resizeMode='cover' source={{url:image}} style={imageStyle}></Image>
             </CardSection>
+
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <Image source={{url:thumbnail}} style={thumbnailStyle}></Image>
+                <View style={infoStyle}>
+                    <Text style={titleStyle}>{title}</Text>
+                    <Text style={desStyle}>{des}</Text>
+                </View>
             </CardSection>
+
+
         </Card>
     )
 }
@@ -25,7 +43,23 @@ const styles = {
         borderTopLeftRadius:3,
         borderTopRightRadius:3,
         overflow:'hidden',
-        
+    },
+    thumbnailStyle: {
+        width:48,
+        height:48,
+        marginRight:8,
+        borderRadius:3,
+    },
+    titleStyle: {
+        fontSize:18,
+        fontWeight:'600',
+        marginBottom:6
+    },
+    des: {
+        fontSize:13
+    },
+    infoStyle: {
+        flex:1,
     }
 }
 export default AlbumDetail;
