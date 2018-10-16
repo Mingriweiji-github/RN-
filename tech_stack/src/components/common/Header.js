9af1dbbd240d188d56d0786cf85f66f83f4a777c
KM <18610929200@163.com>
2018-10-16 16:02:14 +0800

    tech_stack

---
+++ tech_stack/src/components/common/Header.js
@@ -0,0 +1,30 @@
+ import React from 'react';
+ import {Text,View,RCTView} from 'react-native';
+
+ const Header = (props) => {
+    const {textStyle,viewStyle} = styles;
+
+     return (
+         <View style={viewStyle}> 
+            <Text style={textStyle}>{props.headerText}</Text>
+         </View>
+     )
+ }
+
+ const styles = {
+     textStyle: {
+         fontSize:20
+     },
+     viewStyle: {
+         backgroudColor:'#dddddd',
+         height:64,
+         justifyConment:'center',
+         alignItems:'center',
+         paddingTop:15,
+         shadowColor:'#000', 
+         shadowOffset:{width:0,height:2},
+         shadowOpacity:0.3,
+         position:'relative'
+     }
+ }
+ export default Header;
